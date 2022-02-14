66c3acbc333da72a5e4761dff8fd5b38d3c5cedb
misper <85740539+misperception@users.noreply.github.com>
2022-02-14 21:09:23 +0000

    added framework for vappost command and further image posting

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ with open("copypastaes.txt") as textes:
 
 @vap.event
 async def on_ready():
-    print(copypasta)
+  print(copypasta)
 
 @vap.command(name='copypasta')
 async def copy(ctx):
-    await ctx.channel.send(copypasta)
+  await ctx.channel.send(copypasta)
 
 @vap.command(name='copypastaes')
 async def copyes(ctx):
-    await ctx.channel.send(copypastaes)
+  await ctx.channel.send(copypastaes)
 
-
+@vap.command(name='vappost')
+async def vappost(ctx, arg):
+  try:
+    int(arg)
+  except:
+    await ctx.channel.send("Argument is not a valid number")
+  if int(arg) <= 0:
+    await ctx.channel.send("Argument is lower or equal to zero, please choose a positive integer")
+  else:
+    await ctx.channel.send(arg)
 
 token = os.environ['TOKEN']
 vap.run(token)
