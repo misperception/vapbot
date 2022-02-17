32a56cd1a4d74b2bbecc12656b4bab7b4fd5a19f
misperception <victoriuscamver@gmail.com>
2022-02-17 19:21:20 +0100

    added new 'roulette' command (NSFW only)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os, requests, discord, random
+import os, requests, discord, random, time
+from pickle import TRUE
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -12,6 +13,54 @@ with open("copypastaes.txt",'r', encoding='utf-8') as textes:
 @vap.event
 async def on_ready():
   print(copypasta)
+
+@vap.command(name='roulette')
+async def roulette(ctx):
+  if ctx.channel.is_nsfw():
+    shot = random.randrange(1,7)
+    print(f"shot = {shot}")
+    if shot < 6:
+      await ctx.channel.send("You fired a blank, you are safe now.")
+    elif shot == 6:
+      await ctx.channel.send("You fired a shot, you lose, prepare for the second roulette.")
+      time.sleep(1)
+      await ctx.channel.send("Preparing second roulette...")
+      time.sleep(2)
+      roundtwo = random.randrange(1,7)
+      print(f"roundtwo = {roundtwo}")
+      if roundtwo < 6:
+        await ctx.channel.send("You fired a blank, loading normal porn...")
+        baseURL = "https://e621.net/posts.json?page={page}&limit=1"
+        endpoint = baseURL.format(page = str(random.randrange(1,201)))
+        head = {'User-Agent': 'VaporeonBot 1.0.0'}
+        r = requests.get(endpoint, headers=head)
+        jsonpage = r.json()
+        posts = jsonpage['posts']
+        currentpost = posts[0]
+        file = currentpost['file']
+        url = file['url']
+        time.sleep(2)
+        await ctx.send(url)
+      elif roundtwo == 6:
+        await ctx.channel.send("You fired a shot, loading degeneracy... hope you still have faith in humanity after this...")
+        baseURL = "https://e621.net/posts.json?page={page}&limit=1&tags={tags}"
+        tag = ["watersports", "omorashi", "scat", "gore", "vore", "femboy", "anal_vore", "diapers"]
+        randomn = random.randrange(1,len(tag))
+        endpoint = baseURL.format(page = str(random.randrange(1,201)), tags = tag[randomn - 1])
+        head = {'User-Agent': 'VaporeonBot 1.0.0'}
+        r = requests.get(endpoint, headers=head)
+        jsonpage = r.json()
+        posts = jsonpage['posts']
+        currentpost = posts[0]
+        file = currentpost['file']
+        url = file['url']
+        time.sleep(1)
+        await ctx.channel.send("Brace for impact.")
+        time.sleep(2)
+        await ctx.send(url)
+  else:
+    await ctx.channel.send("Make sure to run this command on a NSFW channel.")
+
 
 @vap.command(name='vapcopy')
 async def copyp(ctx):
@@ -36,9 +85,11 @@ async def vapsend(ctx, arg1: discord.Member, arg2):
     await ctx.channel.send("The second argument is not a valid number. (format: v!vapsend {user mention} {number of images})")
   if int(arg2) <= 0:
     await ctx.channel.send("The second argument is lower or equal to zero.")
+  elif int(arg2) >> 100:
+    await ctx.channel.send("The second argument is higher than 100, please choose a lower number")
   else:
     baseURL = "https://e621.net/posts.json?tags=Vaporeon+-animated&limit={number}&page={page}"
-    endpoint = baseURL.format(number = arg2, page = str(random.randrange(1,200)))
+    endpoint = baseURL.format(number = arg2, page = str(random.randrange(1,201)))
     head = {'User-Agent': 'VaporeonBot 1.0.0'}
     r = requests.get(endpoint, headers=head)
     jsonpage = r.json()
@@ -65,9 +116,11 @@ async def vappost(ctx, arg):
     return
   if int(arg) <= 0:
     await ctx.channel.send("Argument is lower or equal to zero, please choose a positive integer.")
+  elif int(arg) >> 100:
+    await ctx.channel.send("The second argument is higher than 100, please choose a lower number")
   else:
     baseURL = "https://e621.net/posts.json?tags=Vaporeon+-animated&limit={number}&page={page}"
-    endpoint = baseURL.format(number = arg, page = str(random.randrange(1,200)))
+    endpoint = baseURL.format(number = arg, page = str(random.randrange(1,201)))
     head = {'User-Agent': 'VaporeonBot 1.0.0'}
     r = requests.get(endpoint, headers=head)
     jsonpage = r.json()
