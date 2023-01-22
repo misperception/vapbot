e038d7862a6c9c37e8f4dbb1fe4db4cfe088442b
misperception <victoriuscamver@gmail.com>
2023-01-22 20:38:17 +0100

    improved vapsend and vappost

--- cogs/trollface.py
+++ cogs/trollface.py
@@ -1,63 +1,51 @@
-import requests, random, discord
+import requests, discord
 from discord.ext import commands
 
 class VaporeonPorn(commands.Cog, name="Trolling"):
+
     def __init__(self, bot):
         self.bot = bot
+
     @commands.command(name='vapsend')
-    async def vapsend(self, ctx, arg1: discord.Member, arg2):
-        if int(arg2) <= 0:
-            await ctx.channel.send("The second argument is lower or equal to zero.")
-        elif int(arg2) > 100:
-            await ctx.channel.send("The second argument is higher than 100, please choose a lower number")
-        else:
-            baseURL = "https://e621.net/posts.json?tags=Vaporeon+-animated&limit={number}&page={page}"
-            endpoint = baseURL.format(number = arg2, page = str(random.randrange(1,201)))
-            head = {'User-Agent': 'VaporeonBot 1.0.0'}
+    async def vapsend(self, ctx, arg1: discord.Member, arg2: int):
+        if arg2 <= 0:
+            await ctx.channel.send(f"{arg2} is lower or equal to zero.")
+            return
+        else: pass
+
+        endpoint = "https://e621.net/posts/random.json?tags=Vaporeon+-animated"
+        head = {'User-Agent': 'VaporeonBot 1.0.0'}
+
+        for i in range(arg2):
             r = requests.get(endpoint, headers=head)
-            jsonpage = r.json()
-            posts = jsonpage['posts']
-            n = len(posts)
-            for index in range(n):
-                print(index + 1)
-                currentpost = posts[index]
-                file = currentpost['file']
-                url = file['url']
-                try:
-                    await arg1.send(url)
-                except:
-                    print("couldn't send URL")
+            url = r.json().get("post").get("file").get("url")
+            try:
+                await arg1.send(url)
+            except:
+                print(f"couldn't send URL in attempt {i+1}")
 
     @commands.command(name='vappost')
-    async def vappost(self, ctx, arg):
-        if not ctx.channel.is_nsfw(): # exit clause
+    async def vappost(self, ctx, arg: int):
+        if not ctx.channel.is_nsfw(): 
             await ctx.send("Make sure to run this command on a NSFW channel.")
             return
+        else: pass
 
-        try:
-            int(arg)
-        except:
-            await ctx.channel.send("Argument is not a valid number.")
-            return
-        if int(arg) <= 0:
+        if arg <= 0:
             await ctx.channel.send("Argument is lower or equal to zero, please choose a positive integer.")
             return
-        baseURL = "https://e621.net/posts.json?tags=Vaporeon+-animated&limit={number}"
-        endpoint = baseURL.format(number = arg)
+        else: pass
+
+        endpoint = "https://e621.net/posts/random.json?tags=Vaporeon+-animated"
         head = {'User-Agent': 'VaporeonBot 1.0.0'}
-        r = requests.get(endpoint, headers=head)
-        jsonpage = r.json()
-        posts = jsonpage['posts']
-        n = len(posts)
-        for index in range(n):
-            print(index + 1)
-            currentpost = posts[index]
-            file = currentpost['file']
-            url = file['url']
+
+        for i in range(arg):
+            r = requests.get(endpoint, headers=head)
+            url = r.json().get("post").get("file").get("url")
             try:
                 await ctx.channel.send(url)
             except:
-                print("couldn't send url")
+                print(f"couldn't send url in attempt {i+1}")
 
 def setup(bot):
     bot.add_cog(VaporeonPorn(bot))
